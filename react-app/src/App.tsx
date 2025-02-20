ce9dec9d982f8c4d04b20512fdc9f42247a42ef8
Naveen Manjappa <NManjappa@manh.com>
2025-02-20 23:19:17 +0000

    react-app Children prop for react nodes

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,13 @@
-import ListGroup from "./components/ListGroup";
-
-let items = ["New York", "Bangalore", "London", "Tokyo","Paris"];
-const handleSelectItem = (item:string) => console.log(item);
+import Alert from "./components/Alert";
 
 function App() {
-  return <div>
-    <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
-  </div>
+  return (
+    <div>
+      <Alert>
+        Hello <span>World!</span>
+      </Alert>
+    </div>
+  );
 }
 
 export default App;
