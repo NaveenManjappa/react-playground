a2a5b1d5281e19a2ee5de7281f653963c421fba8
Naveen Manjappa <NManjappa@manh.com>
2025-02-20 23:55:27 +0000

    react-app Alert component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+  const handleClose = () => setAlertVisibility(false);
+  
   return (
     <div>
-      <Alert>
-        Hello <span>World!</span>
-      </Alert>
-      <Button color='info' onClick={() => console.log('clicked')}>My Button</Button>
+      {alertVisible && (
+        <Alert onClose={handleClose}>
+          Hello <span>World!</span>
+        </Alert>
+      )}
+      <Button color="success" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
