64df7c968e00cdd4eac1c0037e3ca8ee1d5e45a8
Naveen Manjappa <NManjappa@manh.com>
2025-02-20 12:25:20 +0000

    react-demos Few more basic components

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,12 +1,15 @@
-import Greet from "./components/Greet"
-import Add from "./components/Add"
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <>
-  <h1>Hello React</h1>
-  <Greet/>
-  <Add/>
-  </>
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  )
 }
 
 export default  App;
