88f3c88a260005649c211cb702e7de54b01d87c0
Naveen Manjappa <NManjappa@manh.com>
2025-02-20 22:02:08 +0000

    react-app List group component and handling events

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,22 @@
+import { MouseEvent } from "react";
 function ListGroup() {
+  let items = ["New York", "Bangalore", "London", "Tokyo"];
+
+  //Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No Items found!</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li key={index} className="list-group-item" onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
