5aabd286768fcb65afc0d828ca4b497088b20c2c
Naveen Manjappa <NManjappa@manh.com>
2025-02-26 07:35:11 +0000

    react-app Expense tracker - integrating react hook form and zod

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -16,9 +16,10 @@ import ZodForm from "./components/ZodForm";
 import ExpenseTracker from "./components/ExpenseTracker";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import categories from "./expense-tracker/categories";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+
 function App() {
   // const [alertVisible, setAlertVisibility] = useState(false);
   // const handleClose = () => setAlertVisibility(false);
--- react-app/src/expense-tracker/components/ExpenseForm.tsx
+++ react-app/src/expense-tracker/components/ExpenseForm.tsx
@@ -1,27 +1,57 @@
-import React from "react";
-import { categories } from "../../App";
+import { z } from "zod";
+import categories from "../categories";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+const schema = z.object({
+  description: z.string().min(3,{message:'Description should be atleast 3 characters'}).max(50),
+  amount: z.number({invalid_type_error:'Amount is required'}).min(0.01).max(100_000),
+  category: z.enum(categories,{
+    errorMap:() => ({message: 'Category is required'})
+  })
+});
+
+type ExpenseFormData = z.infer<typeof schema>;
 
 const ExpenseForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit(data => console.log(data))}>
       <div className="mb-3">
-        <label htmlFor="description" className="form-label">Description</label>
-        <input id="description" type="text" className="form-control" />
+        <label htmlFor="description" className="form-label">
+          Description
+        </label>
+        <input {...register('description')} id="description" type="text" className="form-control" />
+        {errors.description && <p className="text-danger">{errors.description.message}</p>}
       </div>
 
       <div className="mb-3">
-        <label htmlFor="amount" className="form-label">Amount</label>
-        <input id="amount" type="number" className="form-control" />
+        <label htmlFor="amount" className="form-label">
+          Amount
+        </label>
+        <input {...register('amount',{valueAsNumber:true})} id="amount" type="number" className="form-control" />
+        {errors.amount && <p className="text-danger">{errors.amount.message}</p>}
       </div>
 
       <div className="mb-3">
-        <label htmlFor="category" className="form-label">Category</label>
-        <select id="category" className="form-select">
-        <option value="">All Categories</option>
+        <label htmlFor="category" className="form-label">
+          Category
+        </label>
+        <select {...register('category')} id="category" className="form-select">
+          <option value="">All Categories</option>
           {categories.map((category) => (
-                  <option value={category} key={category}>{category}</option>
-                ))}
+            <option value={category} key={category}>
+              {category}
+            </option>
+          ))}
         </select>
+        {errors.category && <p className="text-danger">{errors.category.message}</p>}
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
