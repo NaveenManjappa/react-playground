310b9a9dcbc2d9486d6cc22642d3daf79c0ad48a
Naveen Manjappa <NManjappa@manh.com>
2025-02-26 07:08:02 +0000

    react-app Expense tracker - building expense form

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -15,7 +15,10 @@ import HookForm from "./components/HookForm";
 import ZodForm from "./components/ZodForm";
 import ExpenseTracker from "./components/ExpenseTracker";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 
+export const categories = ["Groceries", "Utilities", "Entertainment"];
 function App() {
   // const [alertVisible, setAlertVisibility] = useState(false);
   // const handleClose = () => setAlertVisibility(false);
@@ -40,29 +43,34 @@ function App() {
     setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
   };
 
-  const [cart,setCart] = useState({
-    discount:0.1,
-    items:[
-      { id: 1,title:'Product 1',quantity:1},
-      { id:2, title:'Product 2',quantity:1}
-    ]
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const updateCart = () => {
     setCart({
       ...cart,
-      items:cart.items.map(item => item.id === 1 ? { ...item,quantity:item.quantity+1}:item)
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
     });
-  }
+  };
 
-  const [expenses,setExpenses] =useState([
-    { id:1,description:'AAA',amount:10,category:'Utilities'},
-    { id:2,description:'BBB',amount:10,category:'Utilities'},
-    { id:3,description:'CCC',amount:10,category:'Utilities'},
-    { id:4,description:'DDD',amount:10,category:'Utilities'},
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "AAA", amount: 10, category: "Utilities" },
+    { id: 2, description: "BBB", amount: 10, category: "Utilities" },
+    { id: 3, description: "CCC", amount: 10, category: "Utilities" },
+    { id: 4, description: "DDD", amount: 10, category: "Entertainment" },
   ]);
 
-  
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category == selectedCategory)
+    : expenses;
   return (
     <div>
       {/* {alertVisible && (
@@ -107,8 +115,18 @@ function App() {
 
     <ExpenseTracker />
 */}
-  <ExpenseList expenses={expenses} onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id))}/>
-
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
+      <div className="mb-5">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
