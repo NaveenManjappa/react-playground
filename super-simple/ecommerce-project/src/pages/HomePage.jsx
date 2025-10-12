8d309e01d53abdea15ffe3e87d99109a33d28f39
Naveen Manjappa <122058418+NaveenManjappa@users.noreply.github.com>
2025-10-12 20:00:03 +0100

    super-simple EC Lift the cart up and share it between pages

--- super-simple/ecommerce-project/src/pages/HomePage.jsx
+++ super-simple/ecommerce-project/src/pages/HomePage.jsx
@@ -3,18 +3,12 @@ import { useState, useEffect } from "react";
 import { Header } from "../components/Header";
 import "./HomePage.css";
 
-function HomePage() {
-  const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
-
+function HomePage({ cart }) {
+  const [products, setProducts] = useState([]); 
   useEffect(() => {
     axios.get("/api/products").then((response) => {
       setProducts(response.data);
-    });
-
-    axios.get("/api/cart-items").then((response) => {
-      setCart(response.data);
-    });
+    });   
   }, []);
 
   return (
