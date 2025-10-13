0ee6942eac70d8e9c07a34001409c363c929c64b
Naveen Manjappa <122058418+NaveenManjappa@users.noreply.github.com>
2025-10-13 21:37:51 +0100

    super-simple EC Separate into smaller components

--- super-simple/ecommerce-project/src/App.jsx
+++ super-simple/ecommerce-project/src/App.jsx
@@ -1,9 +1,9 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router";
 import axios from "axios";
-import HomePage from "./pages/HomePage";
-import CheckoutPage from "./pages/CheckoutPage";
-import { OrdersPage } from "./pages/OrdersPage";
+import HomePage from "./pages/home/HomePage";
+import CheckoutPage from "./pages/checkout/CheckoutPage";
+import { OrdersPage } from "./pages/orders/OrdersPage";
 import "./App.css";
 import { TrackingPage } from "./pages/TrackingPage";
 
@@ -19,8 +19,8 @@ function App() {
     <>
       <Routes>
         <Route index element={<HomePage cart={cart} />} />
-        <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
-        <Route path="orders" element={<OrdersPage cart ={cart}/>} />
+        <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+        <Route path="orders" element={<OrdersPage cart={cart} />} />
         <Route path="tracking" element={<TrackingPage />} />
       </Routes>
     </>
--- super-simple/ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ super-simple/ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { useEffect, useState, Fragment } from "react";
-import { Header } from "../components/Header";
+import { Header } from "../../components/Header";
 import "./OrdersPage.css";
 import dayjs from "dayjs";
-import { formatMoney } from "../utils/money";
+import { formatMoney } from "../../utils/money";
 
 export function OrdersPage({ cart }) {
   const [orders, setOrders] = useState([]);
@@ -56,9 +56,14 @@ export function OrdersPage({ cart }) {
                             {orderProduct.product.name}
                           </div>
                           <div className="product-delivery-date">
-                            Arriving on: {dayjs(orderProduct.estimatedDeliveryTimeMs).format('MMMM D')}
+                            Arriving on:{" "}
+                            {dayjs(orderProduct.estimatedDeliveryTimeMs).format(
+                              "MMMM D"
+                            )}
                           </div>
-                          <div className="product-quantity">Quantity: {orderProduct.quantity}</div>
+                          <div className="product-quantity">
+                            Quantity: {orderProduct.quantity}
+                          </div>
                           <button className="buy-again-button button-primary">
                             <img
                               className="buy-again-icon"
