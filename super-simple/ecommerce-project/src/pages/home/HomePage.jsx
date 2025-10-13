245d0f6d1ffcbcca5a513c318dd2555c40ddc8d4
Naveen Manjappa <122058418+NaveenManjappa@users.noreply.github.com>
2025-10-13 21:48:45 +0100

    super-simple EC Convert home and checkout to async await

--- super-simple/ecommerce-project/src/pages/home/HomePage.jsx
+++ super-simple/ecommerce-project/src/pages/home/HomePage.jsx
@@ -5,11 +5,13 @@ import "./HomePage.css";
 import { ProductsGrid } from "./ProductsGrid";
 
 function HomePage({ cart }) {
-  const [products, setProducts] = useState([]); 
+  const [products, setProducts] = useState([]);
   useEffect(() => {
-    axios.get("/api/products").then((response) => {
+    const getHomeData = async () => {
+      const response = await axios.get("/api/products");
       setProducts(response.data);
-    });   
+    };
+    getHomeData();
   }, []);
 
   return (
