fdabe5eacc7fb9819deb09702a6d15ea0aa67194
Naveen Manjappa <122058418+NaveenManjappa@users.noreply.github.com>
2025-10-14 22:01:28 +0100

    super-simple EC Create update delivery option feature

--- super-simple/ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ super-simple/ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -5,22 +5,28 @@ import "./Checkoutpage.css";
 import { OrderSummary } from "./OrderSummary";
 import { PaymentSummary } from "./PaymentSummary";
 
-function CheckoutPage({ cart }) {
+function CheckoutPage({ cart, loadCart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
   const [paymentSummary, setPaymentSummary] = useState(null);
+
+  useEffect(() => {
+    const fetchCheckoutData = async () => {
+      const response = await axios.get("api/payment-summary");
+      setPaymentSummary(response.data);
+    };
+    fetchCheckoutData();
+  }, [cart]);
 
   useEffect(() => {
     const fetchCheckoutData = async () => {
       let response = await axios.get(
         "/api/delivery-options?expand=estimatedDeliveryTime"
       );
-      setDeliveryOptions(response.data);
-
-      response = await axios.get("api/payment-summary");
-      setPaymentSummary(response.data);
+      setDeliveryOptions(response.data);     
     };
     fetchCheckoutData();
   }, []);
+
   return (
     <>
       <title>Checkout</title>
@@ -51,7 +57,11 @@ function CheckoutPage({ cart }) {
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
-          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
+          <OrderSummary
+            cart={cart}
+            deliveryOptions={deliveryOptions}
+            loadCart={loadCart}
+          />
 
           <PaymentSummary paymentSummary={paymentSummary} />
         </div>
