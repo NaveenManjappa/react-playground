7df2c8a3f149b7c09b295e2b3f999bc44ffa47e9
Naveen Manjappa <122058418+NaveenManjappa@users.noreply.github.com>
2025-10-16 16:27:27 +0100

    super-simple EC Save build folder in ecommerce backend folder

--- super-simple/ecommerce-project/src/App.jsx
+++ super-simple/ecommerce-project/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router";
 import axios from "axios";
-import HomePage from "./pages/home/HomePage";
+import {HomePage} from "./pages/home/HomePage";
 import CheckoutPage from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from "./pages/orders/OrdersPage";
 import "./App.css";
